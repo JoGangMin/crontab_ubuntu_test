c2000922da784bf45066928938169a245a02f0ea
JoGangMin <er1458@naver.com>
2021-01-29 16:52:28 +0900

    token update

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 # /bin/bash
-curl -X POST --data-urlencode "payload={\"channel\": \"#api_logger\", \"username\": \"webhookbot\", \"text\": \"sh start.\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/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
+curl -X POST --data-urlencode "payload={\"channel\": \"#api_logger\", \"username\": \"webhookbot\", \"text\": \"sh start.\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/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
 
 cd /home/ubu-sub-proc/code/test_crontab
 
 python main.py
 
-curl -X POST --data-urlencode "payload={\"channel\": \"#api_logger\", \"username\": \"webhookbot\", \"text\": \"sh end.\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/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
+curl -X POST --data-urlencode "payload={\"channel\": \"#api_logger\", \"username\": \"webhookbot\", \"text\": \"sh end.\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/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
