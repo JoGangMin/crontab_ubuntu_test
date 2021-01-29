65b80dffa5a2b16fcb676609abfb28c931de3b36
JoGangMin <er1458@naver.com>
2021-01-29 16:30:02 +0900

    chmode update

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
+# /bin/bash
 curl -X POST --data-urlencode "payload={\"channel\": \"#api_logger\", \"username\": \"webhookbot\", \"text\": \"sh start.\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/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
 
 cd /home/ubu-sub-proc/code/test_crontab
